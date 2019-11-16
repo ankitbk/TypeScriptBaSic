1e4ba7560f758a25546ff85867e74b78e21c59fc
aksingh2012 <ankitsingh3697@gmail.com>
2019-11-16 11:51:17 +0530

    arrow func and implicit typing - es6 typescript

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,50 @@
-console.log('Hello TypeScript!');
+const pizzas = [{ name: 'pepperoni', topppings: ['Pepperoni'] }];
+
+// ES5 syntax 
+
+const mappedPizzas1 = pizzas.map(function(pizzas) {
+    return pizzas.name.toUpperCase();
+});
+
+//ES6 syntax => arrow functions
+
+const mappedPizzas2 = pizzas.map(pizzas =>  pizzas.name.toUpperCase());
+
+console.log(mappedPizzas1);
+console.log(mappedPizzas2);
+
+// ES5 syntax 
+
+const pizza1 = {
+    name: 'Blazing inferno',
+    getName: function() {
+        console.log(this);
+    }
+}
+
+// Using setTimeOut 
+
+const pizza2 = {
+    name: 'Blazing inferno',
+    
+    getName: function() {
+        const self = this;
+        setTimeout(function() { console.log(self); }, 100);
+    },
+}
+
+// ES6 syntax 
+
+const pizza3 = {
+    name: 'Blazing inferno',
+    
+    getName: function() {
+        
+        setTimeout(() => { console.log(this.name); }, 100);
+    },
+}
+
+
+console.log(pizza1.getName());
+console.log(pizza2.getName());
+console.log(pizza3.getName());
